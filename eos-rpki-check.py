8c798aa75123fea4338c5ee1c0d68d85fa638d4f
Ben Maddison <benm@workonline.co.za>
2020-03-10 16:30:28 +0200

    add ipv6 support and more useful output options

--- eos-rpki-check.py
+++ eos-rpki-check.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Compare reported origin validation status from EOS."""
 
+import collections
+import ipaddress
 import os
 import re
 import sys
@@ -19,17 +21,23 @@ ROV_STATUS = {
 }
 ROV_STATUS_NAME = {v: k for k, v in ROV_STATUS.items()}
 
+BATCHES = {
+    "ipv4": (str(n)
+             for n in ipaddress.ip_network("0.0.0.0/0").subnets(new_prefix=6)),
+    "ipv6": (str(n)
+             for n in ipaddress.ip_network("2000::/3").subnets(new_prefix=8)),
+}
 
-def fetch_vrp(remote_vrp_file, url, node):
+
+def fetch_vrp(remote_vrp_file, url, node, afi):
     """Fetch the current VRP set."""
     if remote_vrp_file:
         response = requests.get(url, headers={"Accept": "application/json"})
         response.raise_for_status()
         vrp_data = response.json()
     else:
-        cmd = "show bgp rpki roa ipv4"
+        cmd = f"show bgp rpki roa {afi}"
         vrp_data = node.enable([cmd])[0]["result"]
-        print(vrp_data)
     vrp_tree = radix.Radix()
     for roa in vrp_data["roas"]:
         prefix = roa["prefix"]
@@ -75,6 +83,18 @@ def search_covering_roas(vrp_tree, prefix):
     return [roa for roa_set in roa_sets for roa in roa_set.data["roas"]]
 
 
+def compare_roa(roa, origin, length):
+    """Compare origin AS and prefix length against ROA."""
+    if length > roa["maxLength"]:
+        return False
+    roa_origin = roa["asn"]
+    if isinstance(roa_origin, int):
+        roa_origin = f"AS{roa_origin}"
+    if origin != roa_origin:
+        return False
+    return True
+
+
 def compare_ov_state(vrp_tree, prefix, length, origin, status):
     """Compute and compare validation state."""
     if status == ROV_STATUS["notValidated"]:
@@ -84,7 +104,7 @@ def compare_ov_state(vrp_tree, prefix, length, origin, status):
     covering_roas = search_covering_roas(vrp_tree, prefix)
     for roa in covering_roas:
         covered = True
-        if length <= roa["maxLength"] and origin == roa["asn"]:
+        if compare_roa(roa, origin, length):
             expected_status = ROV_STATUS["valid"]
             break
     else:
@@ -99,44 +119,99 @@ def compare_ov_state(vrp_tree, prefix, length, origin, status):
     return True, expected_status, covering_roas
 
 
+def result_line(match, prefix, origin, observed_status, expected_status):
+    """Format result output line."""
+    if match:
+        color = "green"
+    else:
+        color = "red"
+    result = click.style(f"observed: {ROV_STATUS_NAME[observed_status]:14} "
+                         f"expected: {ROV_STATUS_NAME[expected_status]:14}",
+                         fg=color)
+    return f"{prefix:30} {origin:10} {result}"
+
+
+def dump_roas(roas):
+    """Format ROA set for output."""
+    for roa in roas:
+        click.echo(f"    {roa}")
+
+
+def print_results(results):
+    """Print results matrix."""
+    click.echo(f"{'':14}| Expected:")
+    click.echo("Observed:     | {}".format("".join([f"{i:>14}"
+                                                    for i in ROV_STATUS])))
+    for observed, o in ROV_STATUS.items():
+        row = "{:14}| {}".format(observed,
+                                 "".join([result_colored(results, o, e)
+                                          for e in ROV_STATUS.values()]))
+        click.echo(row)
+
+
+def result_colored(results, observed, expected):
+    """Return colored results counter."""
+    counter = results[observed][expected]
+    if not counter:
+        color = "white"
+    else:
+        if observed == expected:
+            color = "green"
+        else:
+            color = "red"
+    return click.style(f"{results[observed][expected]:14}", fg=color)
+
+
 @click.command()
 @click.argument("hostname")
 @click.option("--username", "-u", help="EAPI Username",
               prompt=True, default=lambda: os.environ.get("USER", ""))
 @click.option("--password", "-p", help="EAPI Password",
               prompt=True, hide_input=True)
-@click.option("--remote-vrp-file", "-r", help="Get the VRP set from remote",
+@click.option("--afi", "-a", help="Address family",
+              type=click.Choice(["ipv4", "ipv6"]), default="ipv4")
+@click.option("--print-roas", "-r", help="Print ROAs covering each prefix",
+              is_flag=True)
+@click.option("--print-matches", "-m", help="Print matching prefixes",
+              is_flag=True)
+@click.option("--remote-vrp-file", "-R", help="Get the VRP set from remote",
               is_flag=True)
 @click.option("--vrp-url", help="URL of the JSON serialised VRP set",
               default="https://rpki-vc1.wolcomm.net/api/export.json")
-def main(hostname, username, password, remote_vrp_file, vrp_url):
+def main(hostname, username, password, afi, print_roas, print_matches,
+         remote_vrp_file, vrp_url):
     """Compare EOS validation status to the expected results."""
     passed = 0
     failed = 0
+    results = collections.defaultdict(collections.Counter)
     node = pyeapi.connect(host=hostname, username=username, password=password,
                           return_node=True)
-    vrp_tree = fetch_vrp(remote_vrp_file, vrp_url, node)
-    for n in range(256):
-        cmd = f"show bgp ipv4 unicast {n}.0.0.0/8 longer-prefixes"
+    vrp_tree = fetch_vrp(remote_vrp_file, vrp_url, node, afi)
+    for network in BATCHES[afi]:
+        cmd = f"show bgp {afi} unicast {network} longer-prefixes"
         data = node.enable([cmd])[0]
         for prefix, length, paths in iter_routes(data):
             for origin, status in paths:
                 match, expected, roas = compare_ov_state(vrp_tree, prefix,
                                                          length, origin,
                                                          status)
+                line = result_line(match, prefix, origin, status, expected)
                 if match:
-                    result = click.style(ROV_STATUS_NAME[status], fg="green")
                     passed += 1
+                    if print_matches:
+                        click.echo(line)
+                        if print_roas:
+                            dump_roas(roas)
                 else:
-                    result = click.style(f"{ROV_STATUS_NAME[status]} "
-                                         f"(expected: "
-                                         f"{ROV_STATUS_NAME[expected]})",
-                                         fg="red")
                     failed += 1
-                click.echo(f"{prefix:18} {origin:10} {result}")
+                    click.echo(line)
+                    if print_roas:
+                        dump_roas(roas)
+                results[status][expected] += 1
     click.echo("Comparison results:")
     click.secho(f"  Passed: {passed}", fg="green")
     click.secho(f"  Failed: {failed}", fg="red")
+    print_results(results)
     return
 
 
